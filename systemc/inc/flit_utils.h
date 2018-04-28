dcc393bdd7a05bf630e996725065c1a39e88af89
Saif Abrar <syedsaifabrar@gmail.com>
2018-04-28 05:31:48 -0700

    Methods to extract various fields from a header,body,tail flit
    Signed-off-by: Saif Abrar <syedsaifabrar@gmail.com>

--- systemc/inc/flit_utils.h
+++ systemc/inc/flit_utils.h
@@ -31,6 +31,22 @@ void clear_bits(uint32_t *val, uint32_t start, uint32_t range)
     }
 }
 
+// Return bit-range from 'start' in the value 'val'
+uint32_t get_bit_range(uint32_t val, uint32_t start, uint32_t range)
+{
+    uint32_t i, result=0;
+
+    //for each interation, extract bit at (start+i)
+    //and insert at left-shifted position of 'i' in result.
+    for(i=0 ; i<range; i++)
+    {
+        result |= ((val >> (start+i)) & 1) << i;
+    }
+
+    return result;
+}
+
+
 //Compute even parity of value '*f' and set bit[0]
 void set_parity_bit(unsigned int *f)
 {
@@ -183,4 +199,97 @@ void make_tail_flit(unsigned int *t_flit, unsigned int payld)
 
     set_tailflit_payload(t_flit, payld);
 } //end void make_tail_flit()
+
+////////////////////////////////////////////////////////////////
+////                                                        ////
+//// Methods to get fields from a header, body or tail flit ////
+////                                                        ////
+////////////////////////////////////////////////////////////////
+
+/***************************
+** HEADER flit encoding
+****************************
+Bits        Field
+31,30,29    001 (header flit)
+28-17       Length of the packet, upto 4096 flits.
+16-13       Destination router address
+12-9        Source router address.
+8-1         Packet ID
+0           Parity
+****************************/
+//From header 'flit' return error=1 otherwise get
+//length, destination address, source address, packet ID and parity
+unsigned int parse_header_flit(unsigned int flit, unsigned int *len, unsigned int *dest_adrs, unsigned int *src_adrs, unsigned int *id, unsigned int *p)
+{
+
+    //check flit-type field(bits 31,30,29) to indicate HEADER_FLIT,
+    //return 1 on error
+    unsigned int flit_type = get_bit_range(flit, 29, 3);
+    if(flit_type != HEADER_FLIT)
+    {
+        printf("Error! Got unexpected flit-type(%d) for header-flit type(%d)\n", flit_type, HEADER_FLIT);
+        return 1;
+    }
+
+    *len        = get_bit_range(flit, 17, 28-17+1);
+    *dest_adrs  = get_bit_range(flit, 13, 16-13+1);
+    *src_adrs   = get_bit_range(flit, 9, 12-9+1);
+    *id         = get_bit_range(flit, 1, 8-1+1);
+    *p          = get_bit_range(flit, 0, 1);
+
+    return 0; //No error
+} //end parse_header_flit()
+
+/***************************
+** BODY flit encoding
+****************************
+Bits        Field
+31,30,29    010 (body flit)
+28-1        Payload
+0           Parity
+****************************/
+//From body 'flit' return error=1 otherwise get payload and parity
+unsigned int parse_body_flit(unsigned int flit, unsigned int *payload, unsigned int *p)
+{
+    //check flit-type field(bits 31,30,29) to indicate BODY_FLIT,
+    //return 1 on error
+    unsigned int flit_type = get_bit_range(flit, 29, 3);
+    if(flit_type != BODY_FLIT)
+    {
+        printf("Error! Got unexpected flit-type(%d) for body-flit type(%d)\n", flit_type, BODY_FLIT);
+        return 1;
+    }
+
+    *payload = get_bit_range(flit, 1, 28-1+1);
+    *p       = get_bit_range(flit, 0, 1);
+
+    return 0; //No error
+} //end parse_body_flit()
+
+/***************************
+** TAIL flit encoding
+****************************
+Tail flit:
+Bits        Field
+31,30,29    100 (tail flit)
+28-1        Payload
+0           Parity
+****************************/
+//From tail 'flit' return error=1 otherwise get payload and parity
+unsigned int parse_tail_flit(unsigned int flit, unsigned int *payload, unsigned int *p)
+{
+    //check flit-type field(bits 31,30,29) to indicate TAIL_FLIT,
+    //return 1 on error
+    unsigned int flit_type = get_bit_range(flit, 29, 3);
+    if(flit_type != TAIL_FLIT)
+    {
+        printf("Error! Got unexpected flit-type(%d) for tail-flit type(%d)\n", flit_type, TAIL_FLIT);
+        return 1;
+    }
+
+    *payload = get_bit_range(flit, 1, 28-1+1);
+    *p       = get_bit_range(flit, 0, 1);
+
+    return 0; //No error
+} //end parse_tail_flit()
 
--- systemc/inc/flit_utils_test.c
+++ systemc/inc/flit_utils_test.c
@@ -1,29 +1,59 @@
 #include "flit_utils.h"
+#include <assert.h>
 void main()
 {
     printf("Test flit utils\n");
 
+    ///////////////////////////
+    //   Header flit tests   //
+    ///////////////////////////
     unsigned int h_flit;
+    //make a new header-flit
     make_header_flit(&h_flit, 1, 1, 1, 1);
     printf("\nOrig header-flit: 0x%X\n", h_flit);
-
+    //update ID to 5
     set_headerflit_id(&h_flit, 5);
     printf("header-flit with ID 5: 0x%X\n", h_flit);
+    //verify all fields as expected
+    unsigned int len, dest, src, id, p;
+    parse_header_flit(h_flit, &len, &dest, &src, &id, &p);
+    assert(len  == 1);
+    assert(dest == 1);
+    assert(src  == 1);
+    assert(id   == 5); //updated ID
+    printf("Parsed ID : %d\n", id);
 
+    ///////////////////////////
+    //   Body flit tests     //
+    ///////////////////////////
     unsigned int b_flit;
+    //make a new body-flit
     make_body_flit(&b_flit, 1);
     printf("\nOrig body-flit: 0x%X\n", b_flit);
-
+    //update payload
     int payld = 0x6600;
     set_bodyflit_payload(&b_flit, payld);
     printf("body-flit with payload 0x%X: 0x%X\n", payld, b_flit);
+    //verify all fields as expected
+    unsigned int payload;
+    parse_body_flit(b_flit, &payload, &p);
+    assert(payload  == payld); //updated payload
+    printf("Parsed payload : 0x%X\n", payload);
 
+    ///////////////////////////
+    //   Tail flit tests     //
+    ///////////////////////////
     unsigned int t_flit;
+    //make a new tail-flit
     make_tail_flit(&t_flit, 3);
     printf("\nOrig tail-flit: 0x%X\n", t_flit);
-
+    //update payload
     payld = 0x8000000;
     set_tailflit_payload(&t_flit, payld);
     printf("tail-flit with payload 0x%X: 0x%X\n", payld, t_flit);
+    //verify all fields as expected
+    parse_tail_flit(t_flit, &payload, &p);
+    assert(payload  == payld); //updated payload
+    printf("Parsed payload : 0x%X\n", payload);
 
 }
